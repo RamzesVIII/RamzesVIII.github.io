62b0fe10b9589cef33841f72f7afa8367beb3fcd
Roman <litl_rom@tut.by>
2019-11-09 18:50:27 +0300

    Add practice

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(".dropdown-item").mouseover(function() { $(this).css("background-color", "red"); });
+$(".dropdown-item").mouseout(function() { $(this).css("background-color","white"); });
+
+$(".Task").css("color", "black");
+$(".card-header a").css("color", "orange");
